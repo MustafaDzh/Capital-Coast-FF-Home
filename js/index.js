e0fdee68dc2b50ba687182ccce9baf2189978e54
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-06-12 16:12:21 +0300

    info/advantages final

--- js/index.js
+++ js/index.js
@@ -10,26 +10,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     //Project Statics
     let counters = document.querySelectorAll('.count');
-    let speed = 800;
+    let speed = 300;
     let targets = [100000, 40000, 400]; 
-
+    
     counters.forEach((counter, index) => {
         let target = targets[index];
         let count = 0;
         let increment = target / speed;
-
+    
         let updateCount = function() {
             count += increment;
             if (count < target) {
-                counter.textContent = Math.ceil(count).toLocaleString();
+                counter.textContent = Math.ceil(count).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
                 setTimeout(updateCount, 1);
             } else {
-                counter.textContent = target.toLocaleString(); 
+                counter.textContent = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "); 
             }
         };
         updateCount();
     });
-});
+})      
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     // Initialize Swiper
