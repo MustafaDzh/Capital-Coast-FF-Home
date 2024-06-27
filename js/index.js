a91649330c4d96e2e66e760be644db9d45a0fa0b
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-06-27 16:46:49 +0300

    ready

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    //Project Statics
+
+     // Mobile Navigation
+     const hamburgerBtn = document.querySelector('#check-icon');
+     const mobileNav = document.querySelector('.header-container--mobile');
+     const bars = document.querySelectorAll('.bar');
+ 
+     hamburgerBtn.addEventListener('change', function () {
+         if (this.checked) {
+             mobileNav.classList.add('open');
+             bars.forEach(bar => bar.classList.add('cross'));
+         } else {
+             mobileNav.classList.remove('open');
+             bars.forEach(bar => bar.classList.remove('cross'));
+         }
+     });
+
+    // Project Statics
     let counters = document.querySelectorAll('.count');
     let speed = 100;
     let targets = [100000, 40000, 400];
@@ -22,36 +37,11 @@ document.addEventListener('DOMContentLoaded', function () {
         updateCount();
     });
 
-    const swiper = new Swiper('.gallery-swiper_container', {
-        direction: 'horizontal',
-        loop: false,
-        slidesPerView: 1,
-        navigation: {
-            nextEl: '.gallery-swiper_navigation-next',
-            prevEl: '.gallery-swiper_navigation-prev',
-        },
-        pagination: {
-            el: '.gallery-swiper_pagination--bullets',
-            clickable: true,
-            bulletClass: 'gallery-images_swiper-pagination-bullet',
-            bulletActiveClass: 'swiper-pagination-bullet-active',
-        },
-        on: {
-            slideChange: function () {
-                const currentSlide = swiper.realIndex + 1;
-                const totalSlides = swiper.slides.length;
-
-                document.querySelector('.gallery-swiper_pagination--text .current').textContent = currentSlide;
-                document.querySelector('.gallery-swiper_pagination--text .total').textContent = totalSlides;
-            }
-        }
-    });
-
-    const newSwiper = new Swiper('.hero_swiper-container', {
+      // Swiper for hero section
+      const newSwiper = new Swiper('.hero_swiper-container', {
         loop: true,
         pagination: {
             el: '.hero_swiper-pagination',
-            clickable: true,
         },
         autoplay: {
             delay: 3000,
@@ -63,21 +53,29 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         speed: 1000,
     });
-})
 
-//nav-mobile
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerBtn = document.querySelector('#check-icon');
-    const mobileNav = document.querySelector('.header-container--mobile');
-    const bars = document.querySelectorAll('.bar');
-
-    hamburgerBtn.addEventListener('change', function () {
-        if (this.checked) {
-            mobileNav.classList.add('open');
-            bars.forEach(bar => bar.classList.add('cross'));
-        } else {
-            mobileNav.classList.remove('open');
-            bars.forEach(bar => bar.classList.remove('cross'));
+    // Swiper for gallery
+    const swiper = new Swiper('.gallery-swiper_container', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.gallery-swiper_navigation-next',
+            prevEl: '.gallery-swiper_navigation-prev',
+        },
+        pagination: {
+            el: '.gallery-swiper_pagination--bullets',
+            bulletClass: 'gallery-images_swiper-pagination-bullet',
+            bulletActiveClass: 'swiper-pagination-bullet-active',
+        },
+        on: {
+            slideChange: function () {
+                const currentSlide = swiper.realIndex + 1;
+                const totalSlides = swiper.slides.length;
+    
+                document.querySelector('.gallery-swiper_pagination--text .current').textContent = currentSlide;
+                document.querySelector('.gallery-swiper_pagination--text .total').textContent = totalSlides;
+            }
         }
-    });
+    });    
 });
